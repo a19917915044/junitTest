89a7660a893e26698c3f83a1dac9ba275d64dad2
tzq <18779529352@163.com>
2019-06-18 14:38:49 +0800

    1

--- src/main/java/com/example/demo/controller/ShowController.java
+++ src/main/java/com/example/demo/controller/ShowController.java
@@ -1,8 +1,10 @@
 package com.example.demo.controller;
 
+import java.io.IOException;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Controller;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,9 +14,15 @@ public class ShowController {
 
 	@Autowired
 	private ShowMe showMe;
+	@Autowired
+	private ResourceLoader resourceLoader;
 
 	@RequestMapping("/hello")
-	public String hello() {
+	public String hello() throws IOException {
+
+		Resource resource = resourceLoader.getResource("classpath:application-location.properties");
+		System.out.println(resource.getFile().getAbsolutePath());
+		
 
 		return showMe.getHello();
 	}
